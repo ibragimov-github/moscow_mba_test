089c0a57bc021ecf4a4364a79170a044f4f0e18d
Murad Ibragimov <texet0598@gmail.com>
2022-09-16 11:30:11 +0300

    реализован основной функционал

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -1,37 +1,72 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import styles from './Content.module.scss'
 import axios from 'axios'
-import { InfinitySpin } from 'react-loader-spinner'
+import { ProgressBar } from 'react-loader-spinner'
 import { useState, useEffect } from 'react'
+
 
 
 function Content() {
   const apiKey = 'https://api-moscow-mba.herokuapp.com/products'
   const [showPrograms, setShowPrograms] = useState<object[]>([])
   function getRandomArbitrary(min = 0, max = 95) {
-    return Math.random() * (max - min) + min;
+    const random = Math.floor(Math.random() * (max - min) + min);
+    return random
   }
-  const randomNumber = getRandomArbitrary();
-
   useEffect(() => {
-    axios.get(apiKey).then(programs => {
-      const programList = programs.data.slice(randomNumber, randomNumber + 5)
-      for (let program of programList) {
-        const coupleModules = programs.data.filter((el: any) => {
-          if (el.title === program.title) {
-            return el;
-          }
-        })
-        console.log(coupleModules)
-
+    const random = getRandomArbitrary()
+    const acc: any = []
+    axios.get(apiKey).then(({ data }) => {
+      for (let i of data.slice(63, 68)) {
+        acc.push(i)
+        console.log(i)
+        setShowPrograms(acc)
       }
 
-    }).catch(error => console.error(error))
-  }, [randomNumber])
-
+    })
+  }, [])
   return (
     <div className={styles.content}>
+      {!showPrograms.length ?
+        <div className={styles.loader}>
+          <ProgressBar
+            barColor='red'
+          />
+        </div> :
+        showPrograms.map((el: any) => {
+          return (
+            <div
+              key={Date.now() + getRandomArbitrary()}
+              className={styles.card}
+            >
+              <h2 className={styles['card-title']}>{el.title}</h2>
+              <div className={styles['module-container']}>
+                <div className={styles.first}>
+                  <h3 className={styles['module-title']}>1 Модуль</h3>
+                  <ul className={styles['module-list']}>
+                    {el.specializedSubjects.slice(0, 5).map((listEl: any) => (
+                      <li
+                        key={Date.now() + getRandomArbitrary()}
+                      >{listEl.string}</li>
+                    ))}
+                  </ul>
+                </div>
+                <div className={styles.second}>
+                  <h3 className={styles['module-title']}>2 Модуль</h3>
+                  <ul className={styles['module-list']}>
+                    {el.specializedSubjects.slice(5, 10).map((listEl: any) => (
+                      <li
+                        key={Date.now() + getRandomArbitrary()}
+                      >{listEl.string}</li>
+                    ))}
+                  </ul>
+                </div>
+              </div>
+            </div>
+          )
+        })}
     </div>
-  );
+  )
 }
 
 export default Content;
